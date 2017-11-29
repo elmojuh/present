9a9892634bad409ed947c4931f6d996359e3f3b6
Luiz Fernando (luizfcneto <luizfcneto123@gmail.com>
2017-11-29 14:28:31 -0200

    Merge Conflito

--- js/cadastroLogin.js
+++ js/cadastroLogin.js
@@ -28,25 +28,28 @@ function fazerLogin() {
 	
 	else if(cpfUser.value != ""){
 		alert("CPF inválido.");
-	}
-
-
-	if((senhaUser=="123")&&((senhaConfirmarUser=="123"))&&(loginUser!=""))&&((emailUser=="email@fsi.br"))&&((emailConfirmarUser=="email@fsi.br"))&&(( cpfUser != "")) {
-	//Autenticado
-		localStorage.setItem("nomeUsuario", loginUser);
-		localStorage.setItem("emailUsuario", emailUser);
-		localStorage.setItem("senhaUsuario", senhaUser);
-		localStorage.setItem("cpfUsuario", cpfUser);
-		window.location.href="../index/logado/configuracoes/configuracoes.html";
-		window.location.href="../index/logado/configuracoes/configuracoes-de-perfil.html";
-		window.location.href="../index/logado/configuracoes/configuracoes-de-local.html";
-		window.location.href="../index/logado/avaliacao.html";
-		window.location.href="../index/logado/busca.html";
-		window.location.href="../index/logado/feed.html";
-		window.location.href="../index/logado/persona.html";
-
 	} else {
-	//nao autenticado
-		alert("Dados não conferem.");
+
+		if((senhaUser=="123")&&((senhaConfirmarUser=="123"))&&(loginUser!=""))&&((emailUser=="email@fsi.br"))&&((emailConfirmarUser=="email@fsi.br"))&&(( cpfUser != "")) {
+			//Autenticado
+			localStorage.setItem("nomeUsuario", loginUser);
+			localStorage.setItem("emailUsuario", emailUser);
+			localStorage.setItem("senhaUsuario", senhaUser);
+			localStorage.setItem("cpfUsuario", cpfUser);
+			window.location.href="../index/logado/configuracoes/configuracoes.html";
+			// window.location.href="../index/logado/configuracoes/configuracoes-de-perfil.html";
+			// window.location.href="../index/logado/configuracoes/configuracoes-de-local.html";
+			// window.location.href="../index/logado/avaliacao.html";
+			// window.location.href="../index/logado/busca.html";
+			// window.location.href="../index/logado/feed.html";
+			// window.location.href="../index/logado/persona.html";
+
+		} else {
+			//nao autenticado
+			alert("Dados não conferem.");
+		}
 	}
+
+
+	
 }
